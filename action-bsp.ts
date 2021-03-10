ea66d1f3dae1946fd72fccce8f335b5cbe85da64
michael.zikes <michael.zikes@intauria.com>
2021-03-10 15:40:44 +0100

    Redux

---
+++ action-bsp.ts
@@ -0,0 +1,18 @@
+const myAction = {
+  type: '[FlightBooking] FlightsLoaded',
+  flights: [
+    {
+      id: 99,
+      from: 'London'
+    }
+  ]
+};
+
+flightsLoaded({
+  flights: [
+    {
+      id: 99,
+      from: 'London'
+    }
+  ]
+})
---
+++ apps/flight-app/src/app/flight-booking/+state/flight-booking.actions.ts
@@ -0,0 +1,16 @@
+import { Flight } from '@flight-workspace/flight-lib';
+import { createAction, props } from '@ngrx/store';
+
+/* export const loadFlightBookings = createAction(
+  '[FlightBooking] Load FlightBookings'
+); */
+
+export const flightsLoaded = createAction(
+  '[FlightBooking] Flights loaded',
+  props<{ flights: Flight[] }>()
+);
+
+/* export const loadFlightBookingsFailure = createAction(
+  '[FlightBooking] Load FlightBookings Failure',
+  props<{ error: any }>()
+); */
